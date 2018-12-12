1b2f24c6d4aa37249b116d5245fa9287e9417469
KimMungyu <kimmungyu@gmail.com>
2018-12-12 10:21:38 +0100

    컬러칩 추가

---
+++ wantone.js
@@ -0,0 +1,108 @@
+var toneGray = new Vue({
+	el: '#toneGray',
+	data: {
+		items: [
+			{ colorName: 'Gray 110', colorTone:'gray', colorCode: 'gray-110', colorHex: '#141311'},
+			{ colorName: 'Gray 310', colorTone:'gray', colorCode: 'gray-310', colorHex: '#2E3030'},
+			{ colorName: 'Gray 510', colorTone:'gray', colorCode: 'gray-510', colorHex: '#3D3F3F'},
+			{ colorName: 'Gray 520', colorTone:'gray', colorCode: 'gray-520', colorHex: '#434036'},
+			{ colorName: 'Gray 710', colorTone:'gray', colorCode: 'gray-710', colorHex: '#818281'},
+			{ colorName: 'Gray 720', colorTone:'gray', colorCode: 'gray-720', colorHex: '#76776F'},
+			{ colorName: 'Gray 810', colorTone:'gray', colorCode: 'gray-810', colorHex: '#BAA99F'},
+			{ colorName: 'Gray 820', colorTone:'gray', colorCode: 'gray-820', colorHex: '#DAD3C7'}
+		]
+	}
+});
+var toneBlue = new Vue({
+	el: '#toneBlue',
+	data: {
+		items: [
+			{ colorName: 'Blue 210', colorTone:'blue', colorCode: 'blue-210', colorHex: '#122032'},
+			{ colorName: 'Blue 410', colorTone:'blue', colorCode: 'blue-410', colorHex: '#02275B'},
+			{ colorName: 'Blue 510', colorTone:'blue', colorCode: 'blue-510', colorHex: '#00348C'},
+			{ colorName: 'Blue 610', colorTone:'blue', colorCode: 'blue-610', colorHex: '#006BAE'},
+			{ colorName: 'Blue 620', colorTone:'blue', colorCode: 'blue-620', colorHex: '#017C7C'},
+			{ colorName: 'Blue 630', colorTone:'blue', colorCode: 'blue-630', colorHex: '#4E6278'},
+			{ colorName: 'Blue 710', colorTone:'blue', colorCode: 'blue-710', colorHex: '#3C75AA'},
+			{ colorName: 'Blue 720', colorTone:'blue', colorCode: 'blue-720', colorHex: '#148BA8'},
+			{ colorName: 'Blue 730', colorTone:'blue', colorCode: 'blue-730', colorHex: '#1095B6'},
+			{ colorName: 'Blue 740', colorTone:'blue', colorCode: 'blue-740', colorHex: '#4186A2'},
+			{ colorName: 'Blue 810', colorTone:'blue', colorCode: 'blue-810', colorHex: '#69A1D2'},
+			{ colorName: 'Blue 910', colorTone:'blue', colorCode: 'blue-910', colorHex: '#B3D9CD'}
+		]
+	}
+});
+var toneRed = new Vue({
+	el: '#toneRed',
+	data: {
+		items: [
+			{ colorName: 'Red 310', colorTone:'red', colorCode: 'red-310', colorHex: '#970F15'},
+			{ colorName: 'Red 410', colorTone:'red', colorCode: 'red-410', colorHex: '#B11717'},
+			{ colorName: 'Red 510', colorTone:'red', colorCode: 'red-510', colorHex: '#D24F08'},
+			{ colorName: 'Red 610', colorTone:'red', colorCode: 'red-610', colorHex: '#F52711'},
+			{ colorName: 'Red 710', colorTone:'red', colorCode: 'red-710', colorHex: '#ED5F16'},
+			{ colorName: 'Red 720', colorTone:'red', colorCode: 'red-720', colorHex: '#EC7D17'},
+			{ colorName: 'Red 810', colorTone:'red', colorCode: 'red-810', colorHex: '#FAA20E'},
+			{ colorName: 'Red 820', colorTone:'red', colorCode: 'red-820', colorHex: '#EBBB1E'},
+			{ colorName: 'Red 830', colorTone:'red', colorCode: 'red-830', colorHex: '#D2B20A'},
+			{ colorName: 'Red 910', colorTone:'red', colorCode: 'red-910', colorHex: '#EDCC71'},
+			{ colorName: 'Red 920', colorTone:'red', colorCode: 'red-920', colorHex: '#EFD765'}
+		]
+	}
+});
+var tonePurple = new Vue({
+	el: '#tonePurple',
+	data: {
+		items: [
+			{ colorName: 'Purple 210', colorTone:'purple', colorCode: 'purple-210', colorHex: '#301554'},
+			{ colorName: 'Purple 310', colorTone:'purple', colorCode: 'purple-310', colorHex: '#580B53'},
+			{ colorName: 'Purple 410', colorTone:'purple', colorCode: 'purple-410', colorHex: '#362D77'},
+			{ colorName: 'Purple 420', colorTone:'purple', colorCode: 'purple-420', colorHex: '#A3154C'},
+			{ colorName: 'Purple 430', colorTone:'purple', colorCode: 'purple-430', colorHex: '#7F1A5D'},
+			{ colorName: 'Purple 510', colorTone:'purple', colorCode: 'purple-510', colorHex: '#884994'},
+			{ colorName: 'Purple 520', colorTone:'purple', colorCode: 'purple-520', colorHex: '#41418E'},
+			{ colorName: 'Purple 610', colorTone:'purple', colorCode: 'purple-610', colorHex: '#C53878'},
+			{ colorName: 'Purple 710', colorTone:'purple', colorCode: 'purple-710', colorHex: '#9F81B7'},
+			{ colorName: 'Purple 720', colorTone:'purple', colorCode: 'purple-720', colorHex: '#DE82B4'},
+			{ colorName: 'Purple 810', colorTone:'purple', colorCode: 'purple-810', colorHex: '#D694A8'}
+		]
+	}
+});
+var toneGreen = new Vue({
+	el: '#toneGreen',
+	data: {
+		items: [
+			{ colorName: 'Green 210', colorTone:'purple', colorCode: 'green-210', colorHex: '#173024'},
+			{ colorName: 'Green 410', colorTone:'purple', colorCode: 'green-410', colorHex: '#0E5F27'},
+			{ colorName: 'Green 510', colorTone:'purple', colorCode: 'green-510', colorHex: '#066C33'},
+			{ colorName: 'Green 520', colorTone:'purple', colorCode: 'green-520', colorHex: '#696A3B'},
+			{ colorName: 'Green 610', colorTone:'purple', colorCode: 'green-610', colorHex: '#52765C'},
+			{ colorName: 'Green 620', colorTone:'purple', colorCode: 'green-620', colorHex: '#1D9343'},
+			{ colorName: 'Green 710', colorTone:'purple', colorCode: 'green-710', colorHex: '#32B618'},
+			{ colorName: 'Green 720', colorTone:'purple', colorCode: 'green-720', colorHex: '#8DAB28'},
+			{ colorName: 'Green 810', colorTone:'purple', colorCode: 'green-810', colorHex: '#08D34A'},
+			{ colorName: 'Green 820', colorTone:'purple', colorCode: 'green-820', colorHex: '#C7E500'},
+			{ colorName: 'Green 830', colorTone:'purple', colorCode: 'green-830', colorHex: '#08D34A'},
+			{ colorName: 'Green 910', colorTone:'purple', colorCode: 'green-910', colorHex: '#C2E17C'}
+		]
+	}
+});
+var toneBrown = new Vue({
+	el: '#toneBrown',
+	data: {
+		items: [
+			{ colorName: 'Brown 210', colorTone:'purple', colorCode: 'brown-210', colorHex: '#3F2417'},
+			{ colorName: 'Brown 220', colorTone:'purple', colorCode: 'brown-220', colorHex: '#452318'},
+			{ colorName: 'Brown 310', colorTone:'purple', colorCode: 'brown-310', colorHex: '#581817'},
+			{ colorName: 'Brown 510', colorTone:'purple', colorCode: 'brown-510', colorHex: '#903F1D'},
+			{ colorName: 'Brown 520', colorTone:'purple', colorCode: 'brown-520', colorHex: '#725F42'},
+			{ colorName: 'Brown 610', colorTone:'purple', colorCode: 'brown-610', colorHex: '#685C52'},
+			{ colorName: 'Brown 620', colorTone:'purple', colorCode: 'brown-620', colorHex: '#9D5D33'},
+			{ colorName: 'Brown 630', colorTone:'purple', colorCode: 'brown-630', colorHex: '#80514B'},
+			{ colorName: 'Brown 710', colorTone:'purple', colorCode: 'brown-710', colorHex: '#C47144'},
+			{ colorName: 'Brown 810', colorTone:'purple', colorCode: 'brown-810', colorHex: '#BDA46D'},
+			{ colorName: 'Brown 820', colorTone:'purple', colorCode: 'brown-820', colorHex: '#E9B66F'},
+			{ colorName: 'Brown 830', colorTone:'purple', colorCode: 'brown-830', colorHex: '#DFAB85'}
+		]
+	}
+});
