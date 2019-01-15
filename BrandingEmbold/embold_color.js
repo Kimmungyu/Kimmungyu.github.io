8694d2fd20f89dcfc94f1db73bb0cd91e311a72f
김문규 <kimmungyu@gmail.com>
2019-01-15 17:17:15 +0100

    embold design

---
+++ BrandingEmbold/embold_color.js
@@ -0,0 +1,96 @@
+var neutral = new Vue({
+	el: '#neutral',
+	data: {
+		items: [
+			{ colorName: 'Neutral 900', colorTone:'neutral', colorCode: 'neutral-900', colorHex: '#0f1c3a  /  CMYK 96%, 86%, 46%, 56%'},
+			{ colorName: 'Neutral 800', colorTone:'neutral', colorCode: 'neutral-800', colorHex: '#213351'},
+			{ colorName: 'Neutral 700', colorTone:'neutral', colorCode: 'neutral-700', colorHex: '#2a3e5d'},
+			{ colorName: 'Neutral 600', colorTone:'neutral', colorCode: 'neutral-600', colorHex: '#334968'},
+			{ colorName: 'Neutral 500', colorTone:'neutral', colorCode: 'neutral-500', colorHex: '#506682'},
+			{ colorName: 'Neutral 400', colorTone:'neutral', colorCode: 'neutral-400', colorHex: '#657992'},
+			{ colorName: 'Neutral 300', colorTone:'neutral', colorCode: 'neutral-300', colorHex: '#7a8da2'},
+			{ colorName: 'Neutral 200', colorTone:'neutral', colorCode: 'neutral-200', colorHex: '#8ea0b1'},
+			{ colorName: 'Neutral 100', colorTone:'neutral', colorCode: 'neutral-100', colorHex: '#a3b3c1'},
+			{ colorName: 'Neutral 80', colorTone:'neutral', colorCode: 'neutral-80', colorHex: '#cad5db'},
+			{ colorName: 'Neutral 60', colorTone:'neutral', colorCode: 'neutral-60', colorHex: '#d7dfe3'},
+			{ colorName: 'Neutral 40', colorTone:'neutral', colorCode: 'neutral-40', colorHex: '#e3e8ec'},
+			{ colorName: 'Neutral 20', colorTone:'neutral', colorCode: 'neutral-20', colorHex: '#eff2f4'},
+			{ colorName: 'Neutral 0', colorTone:'neutral', colorCode: 'neutral-0', colorHex: '#ffffff'}
+		]
+	}
+});
+
+var blue = new Vue({
+	el: '#blue',
+	data: {
+		items: [
+			{ colorName: 'Blue 900', colorTone:'blue', colorCode: 'blue-900', colorHex: '#004bb7  /  CMYK 94%, 77%, 0%, 0%'},
+			{ colorName: 'Blue 800', colorTone:'blue', colorCode: 'blue-800', colorHex: '#1b69c7'},
+			{ colorName: 'Blue 700', colorTone:'blue', colorCode: 'blue-700', colorHex: '#3786d6'},
+			{ colorName: 'Blue 600', colorTone:'blue', colorCode: 'blue-600', colorHex: '#52a4e6'},
+			{ colorName: 'Blue 500', colorTone:'blue', colorCode: 'blue-500', colorHex: '#98caee'},
+			{ colorName: 'Blue 400', colorTone:'blue', colorCode: 'blue-400', colorHex: '#bcdcf3'},
+			{ colorName: 'Blue 300', colorTone:'blue', colorCode: 'blue-300', colorHex: '#dfeff7'}
+		]
+	}
+});
+
+var orange = new Vue({
+	el: '#orange',
+	data: {
+		items: [
+			{ colorName: 'Orange 900', colorTone:'orange', colorCode: 'orange-900', colorHex: '#f46c21  /  CMYK 0%, 72%, 98%, 0%'},
+			{ colorName: 'Orange 800', colorTone:'orange', colorCode: 'orange-800', colorHex: '#f58040'},
+			{ colorName: 'Orange 700', colorTone:'orange', colorCode: 'orange-700', colorHex: '#f5935f'},
+			{ colorName: 'Orange 600', colorTone:'orange', colorCode: 'orange-600', colorHex: '#f6a77f'},
+			{ colorName: 'Orange 500', colorTone:'orange', colorCode: 'orange-500', colorHex: '#f6bb9e'},
+			{ colorName: 'Orange 400', colorTone:'orange', colorCode: 'orange-400', colorHex: '#f6cebd'},
+			{ colorName: 'Orange 300', colorTone:'orange', colorCode: 'orange-300', colorHex: '#f7e2dc'}
+		]
+	}
+});
+
+var green = new Vue({
+	el: '#green',
+	data: {
+		items: [
+			{ colorName: 'Green 900', colorTone:'green', colorCode: 'green-900', colorHex: '#0ac940'},
+			{ colorName: 'Green 800', colorTone:'green', colorCode: 'green-800', colorHex: '#2dd15b'},
+			{ colorName: 'Green 700', colorTone:'green', colorCode: 'green-700', colorHex: '#50d876'},
+			{ colorName: 'Green 600', colorTone:'green', colorCode: 'green-600', colorHex: '#73e092'},
+			{ colorName: 'Green 500', colorTone:'green', colorCode: 'green-500', colorHex: '#96e8ad'},
+			{ colorName: 'Green 400', colorTone:'green', colorCode: 'green-400', colorHex: '#b9efc8'},
+			{ colorName: 'Green 300', colorTone:'green', colorCode: 'green-300', colorHex: '#dcf7e3'}
+		]
+	}
+});
+
+var red = new Vue({
+	el: '#red',
+	data: {
+		items: [
+			{ colorName: 'Red 900', colorTone:'red', colorCode: 'red-900', colorHex: '#f52711'},
+			{ colorName: 'Red 800', colorTone:'red', colorCode: 'red-800', colorHex: '#f54735'},
+			{ colorName: 'Red 700', colorTone:'red', colorCode: 'red-700', colorHex: '#f66758'},
+			{ colorName: 'Red 600', colorTone:'red', colorCode: 'red-600', colorHex: '#f6877c'},
+			{ colorName: 'Red 500', colorTone:'red', colorCode: 'red-500', colorHex: '#f6a7a0'},
+			{ colorName: 'Red 400', colorTone:'red', colorCode: 'red-400', colorHex: '#f7c7c3'},
+			{ colorName: 'Red 300', colorTone:'red', colorCode: 'red-300', colorHex: '#f7e7e7'}
+		]
+	}
+});
+
+var purple = new Vue({
+	el: '#purple',
+	data: {
+		items: [
+			{ colorName: 'Purple 900', colorTone:'purple', colorCode: 'purple-900', colorHex: '#7e3fd8'},
+			{ colorName: 'Purple 800', colorTone:'purple', colorCode: 'purple-800', colorHex: '#905add'},
+			{ colorName: 'Purple 700', colorTone:'purple', colorCode: 'purple-700', colorHex: '#a275e2'},
+			{ colorName: 'Purple 600', colorTone:'purple', colorCode: 'purple-600', colorHex: '#b491e8'},
+			{ colorName: 'Purple 500', colorTone:'purple', colorCode: 'purple-500', colorHex: '#c6aced'},
+			{ colorName: 'Purple 400', colorTone:'purple', colorCode: 'purple-400', colorHex: '#d8c7f2'},
+			{ colorName: 'Purple 300', colorTone:'purple', colorCode: 'purple-300', colorHex: '#eae2f7'}
+		]
+	}
+});
