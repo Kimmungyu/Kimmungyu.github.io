e886e68e6307017e571824731dfdbaee10154ce8
김문규 <kimmungyu@gmail.com>
2019-02-18 09:09:59 +0100

    embold files add

--- embold.com/js/main.js
+++ embold.com/js/main.js
@@ -1,10 +1,20 @@
 $(document).ready(function () {
-$('.nav-hover').mouseover(function (e) {
-  e.stopPropagation();
-  $('.nav-sub').addClass('display',1000)
+  $('.nav-hover').mouseover(function (e) {
+    e.stopPropagation();
+    $('.nav').addClass('box-shadow-none')
+    $('.nav-sub').addClass('display',1000)
+  });
+  $('.nav-hover').mouseout(function (e) {
+    e.stopPropagation();
+    $('.nav').removeClass('box-shadow-none')
+    $('.nav-sub').removeClass('display',1000)
+  })
 });
-$('.nav-hover').mouseout(function (e) {
-  e.stopPropagation();
-  $('.nav-sub').removeClass('display',1000)
-})
+
+$(window).on("scroll", function() {
+  if($(window).scrollTop() > 300) {
+    $(".nav").addClass("box-shadow");
+  } else {
+    $(".nav").removeClass("box-shadow");
+  }
 });
