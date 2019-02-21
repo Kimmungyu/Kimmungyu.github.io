363276df0e68dae5dc281af1dc6d561a5044320a
김문규 <kimmungyu@gmail.com>
2019-02-21 15:18:02 +0100

    split menu structure

--- embold.com/js/main.js
+++ embold.com/js/main.js
@@ -1,8 +1,4 @@
-$(".nav-hover").load("../nav.html");
-$(".nav-company").load("../nav-company.html");
-$(".nav-product").load("../nav-product.html");
-$(".nav-resources").load("../nav-resources.html");
-$(".nav-user-stories").load("../nav-user-stories.html");
+
 
 $(window).on("scroll", function() {
   if($(window).scrollTop() > 300) {
@@ -10,4 +6,45 @@ $(window).on("scroll", function() {
   } else {
     $(".nav").removeClass("box-shadow");
   }
+});
+
+$('#menu-item-product').click(function(){
+  $('.nav-sub-solutions').hide();
+  $('.nav-sub-resources').hide();
+  $('.nav-sub-company').hide();
+  $('.nav-item-main-solutions').removeClass('active');
+  $('.nav-item-main-resources').removeClass('active');
+  $('.nav-item-main-company').removeClass('active');
+  $('.nav-item-main-product').toggleClass('active');
+  $('.nav-sub-product').fadeToggle(200);
+});
+$('#menu-item-solutions').click(function(){
+  $('.nav-sub-product').hide();
+  $('.nav-sub-resources').hide();
+  $('.nav-sub-company').hide();
+  $('.nav-item-main-product').removeClass('active');
+  $('.nav-item-main-resources').removeClass('active');
+  $('.nav-item-main-company').removeClass('active');
+  $('.nav-item-main-solutions').toggleClass('active');
+  $('.nav-sub-solutions').fadeToggle(200);
+});
+$('#menu-item-resources').click(function(){
+  $('.nav-sub-product').hide();
+  $('.nav-sub-solutions').hide();
+  $('.nav-sub-company').hide();
+  $('.nav-item-main-solutions').removeClass('active');
+  $('.nav-item-main-product').removeClass('active');
+  $('.nav-item-main-company').removeClass('active');
+  $('.nav-item-main-resources').toggleClass('active');
+  $('.nav-sub-resources').fadeToggle(200);
+});
+$('#menu-item-company').click(function(){
+  $('.nav-sub-product').hide();
+  $('.nav-sub-resources').hide();
+  $('.nav-sub-solutions').hide();
+  $('.nav-item-main-solutions').removeClass('active');
+  $('.nav-item-main-resources').removeClass('active');
+  $('.nav-item-main-product').removeClass('active');
+  $('.nav-item-main-company').toggleClass('active');
+  $('.nav-sub-company').fadeToggle(200);
 });
