89250d43d8f48bf42093c5dda3bb335c4be692b3
김문규 <kimmungyu@gmail.com>
2019-02-26 17:05:41 +0100

    update blue version

---
+++ embold-blue.com/js/main.js
@@ -0,0 +1,13 @@
+$('.nav-index').load('../embold.com/nav.html');
+$('.nav-product').load('../embold.com/nav-product.html');
+$('.nav-solutions').load('../embold.com/nav-solutions.html');
+$('.nav-resources').load('../embold.com/nav-resources.html');
+$('.nav-company').load('../embold.com/nav-company.html');
+
+$(window).on("scroll", function() {
+  if($(window).scrollTop() > 300) {
+    $(".nav").addClass("box-shadow");
+  } else {
+    $(".nav").removeClass("box-shadow");
+  }
+});
--- embold.com/js/main.js
+++ embold.com/js/main.js
@@ -3,6 +3,7 @@ $('.nav-product').load('../embold.com/nav-product.html');
 $('.nav-solutions').load('../embold.com/nav-solutions.html');
 $('.nav-resources').load('../embold.com/nav-resources.html');
 $('.nav-company').load('../embold.com/nav-company.html');
+$('#nav-sub-solutions').load('../embold.com/nav-sub-solutions.html');
 
 $(window).on("scroll", function() {
   if($(window).scrollTop() > 300) {
